cfcd2bd75f86a0672ea9e739dd0544d35913d02b
Nathaniel-Kinzelberg <nathanielkinzelberg@gmail.com>
2024-11-14 04:57:35 -0500

    Third Attempt HW2

--- Collatz.java
+++ Collatz.java
@@ -3,30 +3,37 @@ public class Collatz {
 		
 		int firstNumber = Integer.parseInt(args[0]);
 		int currentNumber = 0;
+		int nOn = 0; // Number Of Numbers
 
-		for (int i = 1; i >= firstNumber; i++){
+		for (int i = 1; i <= firstNumber; i++){
 			
 			currentNumber = i;
 			System.out.print(currentNumber + " ");
 			
-			if(currentNumber == 1){			
+			if(currentNumber == 1){			// checks only the first 1 and creates a specific outcome
 				currentNumber = (currentNumber*3) + 1;			
 					while(currentNumber != 1){		
 						System.out.print(currentNumber + " ");
 							if (currentNumber%2 == 0){
 								currentNumber = currentNumber/2;
-								System.out.print(currentNumber + " ");}}
-			}else{
-				if (currentNumber%2 == 0){
-					currentNumber = currentNumber/2;
-					System.out.print(currentNumber + " ");
-				} else {
-					currentNumber = (currentNumber*3) + 1;
-					System.out.print(currentNumber + " ");
+								System.out.print(currentNumber + " ");
+								non++;
+							}
+							System.out.println("(" + nOn + ")");	
+							}
+			}else{							// for all other cases
+				if (currentNumber%2 == 0){	//find if even
+					currentNumber = currentNumber/2; //proceeds with formula
+					System.out.print(currentNumber + " "); //prints number
+				} else {									//if odd
+					currentNumber = (currentNumber*3) + 1; //proceeds with formula	
+					System.out.print(currentNumber + " "); //prints number
 							}
 					}
+					System.out.println("(" + nOn + ")");
+					nOn = 0;
+					} 			
 					System.out.print("Every one of the first " + firstNumber + " hailstone sequences reached 1.");
-		}
     }
 }
 // 1 (1)
